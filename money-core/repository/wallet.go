ed9d653057bb281e2d6beb2b1269dab07141593f
nhonlv <nhonlv.soict@gmail.com>
2021-11-13 22:21:28 +0700

    Revert "add delete wallet func"
    This reverts commit c49094c6bc06e4fc4618678ae48ff22ebabeb1b6.

--- money-core/repository/wallet.go
+++ money-core/repository/wallet.go
@@ -11,10 +11,9 @@ import (
 type (
 	WalletRepoInterface interface {
 		Create(form *view.WalletForm) (*model.Wallet, error)
-		GetById(id string, userId string) (*model.Wallet, error)
+		GetById(id string,userId string) (*model.Wallet, error)
 		Update(form *view.WalletForm) error
 		List(userId string, limit int, from int) ([]*model.Wallet, error)
-		DeleteById(id string, userId string) error
 	}
 	WalletRepo struct {
 		dbConn *gorm.DB
@@ -33,9 +32,9 @@ func (r *WalletRepo) Create(form *view.WalletForm) (*model.Wallet, error) {
 	return walletModel, nil
 }
 
-func (r *WalletRepo) GetById(id string, userId string) (*model.Wallet, error) {
+func (r *WalletRepo) GetById(id string,userId string) (*model.Wallet, error) {
 	wallet := &model.Wallet{}
-	if err := r.dbConn.First(&wallet, "id=? AND user_id=?", id, userId).Error; err != nil {
+	if err := r.dbConn.First(&wallet, "id=? AND user_id=?", id,userId).Error; err != nil {
 		return nil, errors.Errorf("failed to execute select query: %s", err)
 	}
 	return wallet, nil
@@ -54,17 +53,6 @@ func (r *WalletRepo) Update(form *view.WalletForm) error {
 	newWallet.Id = form.WalletId
 	if err := r.dbConn.Updates(newWallet).Error; err != nil {
 		return errors.Errorf("failed to execute update query: %s", err)
-	}
-	return nil
-}
-
-func (r *WalletRepo) DeleteById(id string, userId string) error {
-	wallet, err0 := r.GetById(id, userId)
-	if err0 != nil {
-		return errors.Errorf("can not find wallet by id: %s, error: %s", id, err0)
-	}
-	if err := r.dbConn.Delete(wallet).Error; err != nil {
-		return errors.Errorf("failed to execute delete query: %s", err)
 	}
 	return nil
 }
