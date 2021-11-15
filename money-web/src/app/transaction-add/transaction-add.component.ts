151427717c58b71da587f4ebe13a007773b8f773
monstertau <trungdungvu172@gmail.com>
2021-11-15 08:27:17 +0700

    save work

--- money-web/src/app/transaction-add/transaction-add.component.ts
+++ money-web/src/app/transaction-add/transaction-add.component.ts
@@ -1,7 +1,6 @@
 import {Component, ElementRef, Input, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {TransactionService} from "../services/transaction.service";
 import {TransactionView} from "../view-model/transactions";
-import {type} from "jquery";
 import {Utils} from "../util/utils";
 import {NzModalRef, NzModalService} from "ng-zorro-antd/modal";
 import {TransactionAddCategoryComponent} from "./transaction-add-category/transaction-add-category.component";
@@ -12,103 +11,107 @@ import {WalletView} from "../view-model/wallet";
 import {Subject} from "rxjs";
 
 @Component({
-  selector: 'app-transaction-add',
-  templateUrl: './transaction-add.component.html',
-  styleUrls: ['./transaction-add.component.css']
+    selector: 'app-transaction-add',
+    templateUrl: './transaction-add.component.html',
+    styleUrls: ['./transaction-add.component.css']
 })
 export class TransactionAddComponent implements OnInit {
-  @Input() transaction = new TransactionView();
-  private readonly destroy$ = new Subject();
-  wallets: WalletView[] = [];
-  @ViewChild('inputElement', {static: false}) inputElement?: ElementRef;
+    @Input() transaction = new TransactionView();
+    private readonly destroy$ = new Subject();
+    wallets: WalletView[] = [];
+    @ViewChild('inputElement', {static: false}) inputElement?: ElementRef;
 
-  constructor(private walletService: WalletService, private transactionService: TransactionService, private modal: NzModalService, private viewContainerRef: ViewContainerRef) {
-  }
-
-  onChangeAmount(value: string): void {
-    let res = value.replace(/\D/g, "");
-
-    if (res === null || res.length <= 0) {
-      this.transaction.amount = 0;
-      this.inputElement!.nativeElement.value = this.getFormatAmount();
-      return;
+    constructor(private walletService: WalletService, private transactionService: TransactionService, private modal: NzModalService, private viewContainerRef: ViewContainerRef) {
     }
-    let numberAmount = parseInt(res);
-    if (numberAmount < 200000000000) {
-      this.transaction.amount = numberAmount
-    }
-    this.inputElement!.nativeElement.value = this.getFormatAmount();
-    console.log(this.transaction);
-  }
 
-  getFormatAmount(): string {
-    return Utils.formatNumber(this.transaction.amount.toString())
-  }
+    onChangeAmount(value: string): void {
+        let res = value.replace(/\D/g, "");
 
-  onChangeNote(value: string): void {
-    this.transaction.note = value;
-    console.log(this.transaction);
-  }
-
-
-  onChangeDate(result: Date): void {
-    this.transaction.transactionDate = result;
-    console.log(this.transaction);
-  }
-
-  ngOnInit(): void {
-    this.walletService.getWalletPaging(0, 100).pipe(takeUntil(this.destroy$))
-      .subscribe(
-        (res) => {
-          res.forEach(element => {
-            let walletView = new WalletView().addWallet(element)
-            if (walletView.id === this.transaction.wallet.id) {
-              walletView.isCurrent = true;
-            }
-            this.wallets.push(walletView);
-          });
-
-        },
-        (err) => {
-          console.log(err)
+        if (res === null || res.length <= 0) {
+            this.transaction.amount = 0;
+            this.inputElement!.nativeElement.value = this.getFormatAmount();
+            return;
         }
-      )
-  }
+        let numberAmount = parseInt(res);
+        if (numberAmount < 200000000000) {
+            this.transaction.amount = numberAmount
+        }
+        this.inputElement!.nativeElement.value = this.getFormatAmount();
+        console.log(this.transaction);
+    }
 
-  addTransaction() {
-    console.log(this.transaction);
-    // this.transactionService.addTransaction(this.transaction.toTransaction())
-  }
+    getFormatAmount(): string {
+        return Utils.formatNumber(this.transaction.amount.toString())
+    }
 
-  onAddCategory() {
-    const modal: NzModalRef = this.modal.create({
-      nzTitle: 'Select Category',
-      nzClassName: "add-transaction-category-modal",
-      nzContent: TransactionAddCategoryComponent,
-      nzViewContainerRef: this.viewContainerRef,
-      nzComponentParams: {},
-      nzWidth: 500,
-      nzFooter: []
-    });
-  }
+    onChangeNote(value: string): void {
+        this.transaction.note = value;
+        console.log(this.transaction);
+    }
 
-  onAddWallet() {
-    const modal: NzModalRef = this.modal.create({
-      nzTitle: 'Select Wallet',
-      nzClassName: "add-transaction-wallet-modal",
-      nzContent: TransactionAddWalletComponent,
-      nzViewContainerRef: this.viewContainerRef,
-      nzComponentParams: {
-        wallets: this.wallets
-      },
-      nzWidth: 500,
-      nzFooter: []
-    });
-  }
 
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+    onChangeDate(result: Date): void {
+        this.transaction.transactionDate = result;
+        console.log(this.transaction);
+    }
+
+    ngOnInit(): void {
+        this.walletService.getWalletPaging(0, 100).pipe(takeUntil(this.destroy$))
+            .subscribe(
+                (res) => {
+                    res.forEach(element => {
+                        let walletView = new WalletView().addWallet(element)
+                        if (walletView.id === this.transaction.wallet.id) {
+                            walletView.isCurrent = true;
+                        }
+                        this.wallets.push(walletView);
+                    });
+
+                },
+                (err) => {
+                    console.log(err)
+                }
+            )
+    }
+
+    addTransaction() {
+        console.log(this.transaction);
+        // this.transactionService.addTransaction(this.transaction.toTransaction())
+    }
+
+    onAddCategory() {
+        const modal: NzModalRef = this.modal.create({
+            nzTitle: 'Select Category',
+            nzClassName: "add-transaction-category-modal",
+            nzContent: TransactionAddCategoryComponent,
+            nzViewContainerRef: this.viewContainerRef,
+            nzComponentParams: {},
+            nzWidth: 500,
+            nzFooter: []
+        });
+    }
+
+    onAddWallet() {
+        const modal: NzModalRef = this.modal.create({
+            nzTitle: 'Select Wallet',
+            nzClassName: "add-transaction-wallet-modal",
+            nzContent: TransactionAddWalletComponent,
+            nzViewContainerRef: this.viewContainerRef,
+            nzComponentParams: {
+                wallets: this.wallets
+            },
+            nzWidth: 500,
+            nzBodyStyle: {
+                "padding": "0",
+            },
+            nzFooter: [],
+
+        });
+    }
+
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 
 }
