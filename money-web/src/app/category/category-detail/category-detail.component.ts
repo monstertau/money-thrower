a5f97c1f774359dbd5b1b7e5994ca6766f0a79a0
nguyenbim <51375333+nguyenbim@users.noreply.github.com>
2021-11-18 00:07:49 +0700

    update category component

--- money-web/src/app/category/category-detail/category-detail.component.ts
+++ money-web/src/app/category/category-detail/category-detail.component.ts
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+=======
+import { Component, OnDestroy, OnInit, EventEmitter, Output } from '@angular/core';
+>>>>>>> 9a8fdba3 (update category component)
 import jwtDecode from 'jwt-decode';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AuthService, UserDetail } from 'src/app/services/auth.service';
+<<<<<<< HEAD
 import { CategoryService } from 'src/app/services/category.service';
 import { CommonService } from 'src/app/services/common.service';
 import { Utils } from 'src/app/util/utils';
@@ -197,4 +202,140 @@ export class CategoryDetailComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
+=======
+import { Wallet, WalletService } from 'src/app/services/wallet.service';
+
+
+@Component({
+    selector: 'app-wallet-detail',
+    templateUrl: './wallet-detail.component.html',
+    styleUrls: ['./wallet-detail.component.css']
+})
+export class WalletDetailComponent implements OnInit, OnDestroy {
+
+    @Output() editWallet = new EventEmitter<string>();
+    walletList: Wallet[] = [];
+    data: Wallet[] = [];
+
+    selectedWallet: Wallet = {
+        id: "",
+        name: "",
+        type: 0,
+        currency: "",
+        balance: 0,
+        icon: ""
+    };
+
+    private readonly destroy$ = new Subject();
+
+    currentUser: UserDetail;
+
+    pageSize: number = 4;
+
+    pageOffset: number = 0;
+
+    isListLoading: boolean = false;
+
+    isDetailLoading: boolean = true;
+
+    canLoadMore: boolean = true;
+
+    fallbackIcon = 'assets/catalogs/wallet_icon.png';
+
+    get isListEmpty(): boolean {
+        return this.walletList.length <= 0
+    }
+
+    constructor(private walletService: WalletService, private authService: AuthService) {
+        this.currentUser = jwtDecode(this.authService.userDetail.token);
+    }
+
+    ngOnInit() {
+        this.loadWalletList();
+        this.data = this.walletList;
+    }
+
+    loadMore() {
+        this.pageOffset += this.pageSize;
+        this.loadWalletList();
+    }
+
+    editWalletDetail() {
+        this.editWallet.emit();
+    }
+
+    loadWalletList() {
+        this.isListLoading = true;
+        this.walletService.getWalletPaging(this.pageOffset, this.pageSize)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(
+                (res) => {
+                    if (!res || res.length <= 0) {
+                        this.canLoadMore = false;
+                    }
+                    res.forEach(element => {
+                        this.walletList.push(element);
+                    });
+                },
+                (err) => {
+                    console.log(err)
+                },
+                () => {
+                    this.isListLoading = false;
+                }
+            )
+    }
+
+    loadWalletDetail(id: string) {
+        this.isDetailLoading = true;
+        this.walletService.getWalletById(id)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(
+                (res) => {
+                    this.selectedWallet = res;
+                },
+                (err) => {
+                    console.log(err)
+                },
+                () => {
+                    this.isDetailLoading = false;
+                }
+            )
+    }
+
+    selectWallet(id: string) {
+        let dialog = document.getElementsByClassName('dialog') as HTMLCollectionOf<HTMLElement>;
+        let dialogDetail = document.getElementById('dialog-detail') as HTMLElement;
+        if (dialog.length != 0 && dialog[0].style.marginLeft != '21%' && dialogDetail.hidden) {
+            dialog[0].style.marginLeft = "21%";
+            setTimeout(() => {
+                dialogDetail.hidden = false;
+            }, 500);
+        }
+
+        this.loadWalletDetail(id);
+    }
+
+    hideWalletDetail() {
+        let dialog = document.getElementsByClassName('dialog') as HTMLCollectionOf<HTMLElement>;
+        let dialogDetail = document.getElementById('dialog-detail') as HTMLElement;
+        if (dialog.length != 0 && dialog[0].style.marginLeft == '21%' && !dialogDetail.hidden) {
+            dialog[0].style.marginLeft = "50%";
+            dialogDetail.hidden = true;
+        }
+    }
+
+    formatCurrency(balance: number) {
+        return balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    getIcon(icon: string) {
+        return "assets/catalogs/" + icon + ".png";
+    }
+
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+>>>>>>> 9a8fdba3 (update category component)
 }
--- money-web/src/app/services/category.service.ts
+++ money-web/src/app/services/category.service.ts
@@ -1,17 +1,28 @@
+<<<<<<< HEAD
 import {Injectable} from '@angular/core';
 import {HttpService} from "./http.service";
 import {Category} from "../view-model/category";
+=======
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpService } from './http.service';
+>>>>>>> 9a8fdba3 (update category component)
 
 @Injectable({
     providedIn: 'root'
 })
 export class CategoryService {
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9a8fdba3 (update category component)
     private route = "category"
 
     constructor(private httpService: HttpService) {
     }
 
+<<<<<<< HEAD
     getAllCategory() {
         return this.httpService.get<Category[]>(this.route)
     }
@@ -20,3 +31,48 @@ export class CategoryService {
         return this.httpService.get<Category>(`${this.route}/${id}`);
     }
 }
+=======
+    addCategory(data: CategoryRequest) {
+        return new Observable<string>((observable) => {
+            this.httpService.post<CategoryResponse>('category', data).subscribe((response: CategoryResponse) => {
+                if (!response) {
+                    observable.next('FAIL');
+                } else {
+                    console.log(response)
+                    observable.next('SUCCESS');
+                }
+            }, (error) => {
+                observable.error(error);
+            });
+            return {
+                unsubscribe() {
+                }
+            };
+        })
+    }
+
+    getCategoryPaging(offset: number = 0, limit: number = 10) {
+        return this.httpService.get<Category[]>(this.route, {
+            params: {
+                from: offset,
+                limit: limit
+            }
+        })
+    }
+
+    getCategoryById(id: string) {
+        return this.httpService.get<Category>(`${this.route}/${id}`)
+    }
+}
+
+export interface Category {
+
+}
+
+export interface CategoryRequest {
+}
+
+export interface CategoryResponse {
+}
+
+>>>>>>> 9a8fdba3 (update category component)
