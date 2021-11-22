59fbd8a720ce570f7ac9d9623aa1b7a5804cd808
Bùi Tú Hoàng <64259366+buituhoang@users.noreply.github.com>
2021-11-22 22:26:57 +0700

    Delete transaction (#84)

--- money-web/src/app/services/transaction.service.ts
+++ money-web/src/app/services/transaction.service.ts
@@ -41,6 +41,22 @@ export class TransactionService {
 
 
     }
+
+    deleteTransaction(id: string) {
+        return this.httpService.delete<Transaction>(this.route, id).pipe(
+            catchError((error: HttpErrorResponse) => {
+                if (error.error instanceof Error) {
+                    // A client-side or network error occurred. Handle it accordingly.
+                    console.error('An error occurred:', error.error.message);
+                } else {
+                    // The backend returned an unsuccessful response code.
+                    // The response body may contain clues as to what went wrong,
+                    console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+                }
+                return throwError(error);
+            })
+        );
+    }
 }
 
 export interface Transaction2 {
--- money-web/src/app/transaction/transaction-detail/transaction-detail.component.ts
+++ money-web/src/app/transaction/transaction-detail/transaction-detail.component.ts
@@ -1,37 +1,92 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Transaction2 } from 'src/app/services/transaction.service';
-import { Utils } from 'src/app/util/utils';
-import { TransactionView } from 'src/app/view-model/transactions';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { CommonService } from 'src/app/services/common.service';
+import {Transaction2, TransactionService} from 'src/app/services/transaction.service';
+import {Utils} from 'src/app/util/utils';
+import {TransactionView} from 'src/app/view-model/transactions';
 
 @Component({
-  selector: 'app-transaction-detail',
-  templateUrl: './transaction-detail.component.html',
-  styleUrls: ['./transaction-detail.component.css']
+    selector: 'app-transaction-detail',
+    templateUrl: './transaction-detail.component.html',
+    styleUrls: ['./transaction-detail.component.css']
 })
 export class TransactionDetailComponent implements OnInit {
-  @Input() transaction!: TransactionView;
-  @Output() closed = new EventEmitter<boolean>();
-  constructor() { }
+    @Input() transaction!: TransactionView;
+    @Output() closed = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
-  }
+    transactionDeleteLoading: boolean = false;
 
-  closeDetail() {
-    this.closed.emit(true);
-    let dialog = document.getElementById('transaction-detail') as HTMLElement;
-    dialog.hidden = true;
-    let dialogList = document.getElementsByClassName('list-transaction') as HTMLCollectionOf<HTMLElement>;
-    if (dialogList.length > 0) {
-      dialogList[0].style.marginLeft = '50%';
+    showDeleteModal: boolean = false;
+
+    constructor(private notification: NzNotificationService, private transactionService: TransactionService, private commonService: CommonService) {
     }
-  }
 
-  getDate(date: string) {
-    return Utils.getDate(date);
-  }
+    ngOnInit(): void {
+    }
 
-  getFormatBalance(balance: number) {
-    return Utils.formatCurrency(balance);
-  }
+    closeDetail() {
+        this.closed.emit(true);
+        let dialog = document.getElementById('transaction-detail') as HTMLElement;
+        dialog.hidden = true;
+        let dialogList = document.getElementsByClassName('list-transaction') as HTMLCollectionOf<HTMLElement>;
+        if (dialogList.length > 0) {
+            dialogList[0].style.marginLeft = '50%';
+        }
+    }
+
+    getDate(date: string) {
+        return Utils.getDate(date);
+    }
+
+    getFormatBalance(balance: number) {
+        return Utils.formatCurrency(balance);
+    }
+
+    showTransactionDeleteModal() {
+        this.showDeleteModal = true;
+    }
+
+    handleCancelDelete() {
+        this.showDeleteModal = false;
+    }
+
+    handleDelete() {
+        this.transactionDeleteLoading = true;
+        this.deleteTransaction()
+            .then(() => {
+                setTimeout(() => {
+                    // reset
+                    this.transactionDeleteLoading = false;
+                    this.showDeleteModal = false;
+                    this.commonService.reloadComponent();
+                }, 1000)
+
+            })
+            .catch(error => {
+                this.transactionDeleteLoading = false;
+                this.showErrorMessage(error.toString())
+            })
+    }
+
+    async deleteTransaction() {
+        let error = null;
+        this.transactionService.deleteTransaction(this.transaction.id).subscribe(
+            result => {
+                console.log(result);
+            },
+            err => {
+                console.log(err);
+                error = new Error("Something wrong. please")
+            }
+        )
+        if (error !== null) {
+            throw error
+        }
+
+    }
+
+    showErrorMessage(message: string) {
+        this.notification.error('Error', message);
+    }
 
 }
