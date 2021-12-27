320460cb9a3b131ad0db6f363c83629ab0d7b93f
nguyenbim <51375333+nguyenbim@users.noreply.github.com>
2021-12-27 23:09:33 +0700

    load data

--- money-web/src/app/components/outcome-pie-chart/outcome-pie-chart.component.ts
+++ money-web/src/app/components/outcome-pie-chart/outcome-pie-chart.component.ts
@@ -5,6 +5,8 @@ import { CommonService } from 'src/app/services/common.service';
 import { TransactionView } from '../../view-model/transactions';
 import { CategoryService } from '../../services/category.service';
 import { WalletService } from '../../services/wallet.service';
+import {DataRange, DataUnit} from "../../view-model/data-range";
+import * as moment from "moment";
 
 
 
@@ -15,10 +17,10 @@ import { WalletService } from '../../services/wallet.service';
 })
 export class OutcomePieChartComponent implements OnInit {
 
-    test = {};
-    tmp= {};
-
-    multi!: any[];
+    @Input() dataRange!: DataRange;
+    @Input() startDate!: Date;
+    @Input() endDate!: Date;
+    @Input() multi: DataPoint[] = [];
     @Input() view?: any[];
     // options
     animations: boolean = true;
@@ -26,70 +28,39 @@ export class OutcomePieChartComponent implements OnInit {
         domain: ['#597C2B', '#668D2E', '#80A142', '#91B247', '#D2E459']
       };
 
-    transactions: TransactionView[] = [];
-    currentWalletId!: string;
-    filter: TransactionFilter = {
-        cat_id: '',
-        wallet_id: '',
-        end_amount: 0,
-        start_amount: 0,
-        start_date: 0,
-        end_date: 0,
-        key_note: '',
-    };
+    
 
-    constructor(private transactionService: TransactionService, private categoryService: CategoryService, private walletService: WalletService, private commonService: CommonService) {     
-        this.commonService.currentWallet.subscribe(wallet => { this.currentWalletId = wallet; });
-        this.filter.wallet_id = this.currentWalletId;
-
-        this.test = {
-            "name" : "cba",
-            "value" : 2000
-        };
-  
-        Object.assign(this, {multi});
-       
-        // while(this.multi.length>0){
-        //     this.multi.pop();
-        // }
-
-        this.filter.cat_id = '';
-        this.filter.start_date = 0;
-        this.filter.end_date = 0;
-
-        // this.transactionService.getTransactions(this.filter).subscribe(transactions => {
-        //     transactions.forEach(transaction => {
-        //         this.tmp = {
-        //             "name": transaction.cat_id,
-        //             "value": transaction.amount
-        //         };
-        //         this.multi.push(this.tmp); 
-        //     })
-        //   }, (err) => {
-        //     console.log(err)
-        //   }
-        // );
-        // for(let i=0; i<9; i++){
-        //     this.test = {
-        //         "name" : i.toString(),
-        //         "value": i*1000
-        //     };
-
-        //     this.multi.push(this.test);
-
-        // }
-
-        // console.log(this.multi);
-
-    }
+    constructor() {}
 
     ngOnInit(): void {
+        // if (this.dataRange.dataUnits.length) {
+        //     for (let dataUnit of this.dataRange.dataUnits) {
+        //         let outcome = 0;
+        //         for (let outcomeTrans of dataUnit.outcomeTransaction) {
+        //             outcome += outcomeTrans.amount
+        //         }
+        //         let dataPoint = {
+        //             name: dataUnit.name,
+        //             value: outcome
+        //         }
+        //         this.multi.push(dataPoint)
+        //     }
+        //     console.log(this.multi);
+        // }
+    }
+
+
+
+    onSelect(event: any) {
+        console.log(event);
     }
 
     
 
-
-    
-
-
 }
+
+interface DataPoint {
+    name: string;
+    value: number;
+}
+
